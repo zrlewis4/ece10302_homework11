70238eacadea9b4997bba385c7dfd21f94acf31f
Zachary Lewis <zlewis@mail.bradley.edu>
2018-11-27 16:20:24 -0600

    Completion of Homework 11

--- ece103_homework11/external_data.cpp
+++ ece103_homework11/external_data.cpp
@@ -3,7 +3,7 @@
 char nickname_list[25][30] = {
     "Dr. Mali*.", 
 //* Do not modify above *
-    "My nick name",
+    "Zachary Lewis",
 //* Do not modify below *
     "" // the last enrtry and all potential remaining entires are empty strings
 };
